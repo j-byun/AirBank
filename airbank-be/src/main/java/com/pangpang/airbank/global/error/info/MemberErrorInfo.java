05a01cb76df8b2a3aac9794acbc06c4f7bd88f0b
jjuny7712 <jjuny7712@gmail.com>
2023-09-14 11:02:32 +0900

    feat: 사용자 관련 예외 메시지 추가

--- airbank-be/src/main/java/com/pangpang/airbank/global/error/info/MemberErrorInfo.java
+++ airbank-be/src/main/java/com/pangpang/airbank/global/error/info/MemberErrorInfo.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 
 @Getter
 public enum MemberErrorInfo {
-	NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, 100, "사용자를 찾을 수 없습니다.");
+	NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, 1501, "사용자를 찾을 수 없습니다."),
+	NOT_FOUND_MEMBER_BY_PHONE_NUMBER(HttpStatus.NOT_FOUND, 1502, "휴대폰 번호로 등록된 사용자가 없습니다.");
 
 	private final HttpStatus status;
 	private final Integer code;
