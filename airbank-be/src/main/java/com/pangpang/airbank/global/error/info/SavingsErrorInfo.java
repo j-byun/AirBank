7a0c2bd24f731e4fbb1adc81ce7d0d0c921b6718
jjuny7712 <jjuny7712@gmail.com>
2023-09-22 19:04:29 +0900

    feat: 티끌모으기 관련 예외 처리

---
+++ airbank-be/src/main/java/com/pangpang/airbank/global/error/info/SavingsErrorInfo.java
@@ -0,0 +1,21 @@
+package com.pangpang.airbank.global.error.info;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+
+@Getter
+public enum SavingsErrorInfo {
+	NOT_FOUND_SAVINGS_IN_PROCEEDING(HttpStatus.NOT_FOUND, 1800, "진행 중인 티끌모으기를 찾을 수 없습니다."),
+	NOT_FOUND_SAVINGS_ITEM(HttpStatus.NOT_FOUND, 1801, "티끌 모으기 상품 정보를 찾을 수 없습니다.");
+
+	private final HttpStatus status;
+	private final Integer code;
+	private final String message;
+
+	SavingsErrorInfo(HttpStatus status, Integer code, String message) {
+		this.status = status;
+		this.code = code;
+		this.message = message;
+	}
+}
