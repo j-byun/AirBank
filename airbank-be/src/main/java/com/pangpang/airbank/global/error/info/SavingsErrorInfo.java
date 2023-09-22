e2c43f523ac9668c8405df00cf904fec1c2f57fc
jjuny7712 <jjuny7712@gmail.com>
2023-09-22 20:11:32 +0900

    feat: 티끌모으기 관련 예외 메시지 추가

--- airbank-be/src/main/java/com/pangpang/airbank/global/error/info/SavingsErrorInfo.java
+++ airbank-be/src/main/java/com/pangpang/airbank/global/error/info/SavingsErrorInfo.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 @Getter
 public enum SavingsErrorInfo {
 	NOT_FOUND_SAVINGS_IN_PROCEEDING(HttpStatus.NOT_FOUND, 1800, "진행 중인 티끌모으기를 찾을 수 없습니다."),
-	NOT_FOUND_SAVINGS_ITEM(HttpStatus.NOT_FOUND, 1801, "티끌 모으기 상품 정보를 찾을 수 없습니다.");
+	NOT_FOUND_SAVINGS_ITEM(HttpStatus.NOT_FOUND, 1801, "티끌모으기 상품 정보를 찾을 수 없습니다."),
+	ENROLL_SAVINGS_PERMISSION_DENIED(HttpStatus.FORBIDDEN, 1802, "티끌모으기는 자녀만 등록할 수 있습니다.");
 
 	private final HttpStatus status;
 	private final Integer code;
