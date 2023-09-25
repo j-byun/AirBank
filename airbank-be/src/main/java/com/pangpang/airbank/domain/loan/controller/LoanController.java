603a69511843d575e86338568d8e98f10d8e8dfd
jjuny7712 <jjuny7712@gmail.com>
2023-09-25 13:22:40 +0900

    feat: 땡겨쓰기 조회 기능 구현

--- airbank-be/src/main/java/com/pangpang/airbank/domain/loan/controller/LoanController.java
+++ airbank-be/src/main/java/com/pangpang/airbank/domain/loan/controller/LoanController.java
@@ -1,7 +1,39 @@
 package com.pangpang.airbank.domain.loan.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pangpang.airbank.domain.loan.dto.GetLoanResponseDto;
+import com.pangpang.airbank.domain.loan.service.LoanService;
+import com.pangpang.airbank.global.common.response.EnvelopeResponse;
+import com.pangpang.airbank.global.resolver.dto.AuthenticatedMemberArgument;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@RequestMapping("/loans")
+@RequiredArgsConstructor
+@Slf4j
+@Tag(name = "loans", description = "땡겨쓰기 API")
 public class LoanController {
+
+	private final LoanService loanService;
+
+	// @CheckGroup
+	@GetMapping()
+	public ResponseEntity<EnvelopeResponse<GetLoanResponseDto>> getLoan(@RequestParam("group_id") Long groupId) {
+		AuthenticatedMemberArgument member = new AuthenticatedMemberArgument(2L);
+
+		return ResponseEntity.ok()
+			.body(EnvelopeResponse.<GetLoanResponseDto>builder()
+				.code(HttpStatus.OK.value())
+				.data(loanService.getLoan(member.getMemberId(), groupId))
+				.build());
+	}
 }
