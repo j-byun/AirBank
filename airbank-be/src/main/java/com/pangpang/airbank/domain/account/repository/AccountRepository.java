43672e3e94efa02f991c3c8f3f76f4adb55ad815
jjuny7712 <jjuny7712@gmail.com>
2023-09-30 17:16:59 +0900

    fix: 티끌모으기 가상 계좌가 이전에 생성되었으면 추가로 생성되지 않도록 수정

--- airbank-be/src/main/java/com/pangpang/airbank/domain/account/repository/AccountRepository.java
+++ airbank-be/src/main/java/com/pangpang/airbank/domain/account/repository/AccountRepository.java
@@ -14,8 +14,10 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
 	Optional<Account> findByAccountNumber(String accountNumber);
 
 	Optional<Account> findByMemberIdAndType(Long memberId, AccountType type);
-
+	
 	Optional<Account> findFirstByMemberIsNullAndType(AccountType type);
 
 	Optional<Account> findByMemberAndType(Member member, AccountType type);
+
+	Optional<Account> findFirstByMemberIsNullOrMemberIdAndType(Long memberId, AccountType type);
 }
