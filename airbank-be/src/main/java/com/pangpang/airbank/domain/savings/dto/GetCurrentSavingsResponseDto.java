7fc1b58f62df2805038ba36806f22e6137a0d5e0
jjuny7712 <jjuny7712@gmail.com>
2023-09-30 19:26:14 +0900

    fix: 티끌모으기 현재 내역 조회 응답 데이터 필드 추가

--- airbank-be/src/main/java/com/pangpang/airbank/domain/savings/dto/GetCurrentSavingsResponseDto.java
+++ airbank-be/src/main/java/com/pangpang/airbank/domain/savings/dto/GetCurrentSavingsResponseDto.java
@@ -14,6 +14,7 @@ public class GetCurrentSavingsResponseDto {
 	private Long id;
 	private Long myAmount;
 	private Long parentsAmount;
+	private Long totalAmount;
 	private Long monthlyAmount;
 	private Long thisMonthAmount;
 	private LocalDate startedAt;
@@ -29,6 +30,7 @@ public class GetCurrentSavingsResponseDto {
 			.id(savings.getId())
 			.myAmount(savings.getMyAmount())
 			.parentsAmount(savings.getParentsAmount())
+			.totalAmount(savings.getTotalAmount())
 			.monthlyAmount(savings.getMonthlyAmount())
 			.thisMonthAmount(savings.getAmountThisMonth())
 			.startedAt(savings.getStartedAt())
