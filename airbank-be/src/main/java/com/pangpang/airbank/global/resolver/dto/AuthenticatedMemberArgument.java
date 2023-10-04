5926c025473711e752c39af291d264b0d9bc4c42
jjuny7712 <jjuny7712@gmail.com>
2023-10-04 13:48:48 +0900

    fix: AuthenticatedMemberArgument 스웨거에서 안보이도록 수정

--- airbank-be/src/main/java/com/pangpang/airbank/global/resolver/dto/AuthenticatedMemberArgument.java
+++ airbank-be/src/main/java/com/pangpang/airbank/global/resolver/dto/AuthenticatedMemberArgument.java
@@ -1,5 +1,6 @@
 package com.pangpang.airbank.global.resolver.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(accessMode = Schema.AccessMode.READ_ONLY)
 public class AuthenticatedMemberArgument {
 	private Long memberId;
 
