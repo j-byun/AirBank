8fe82e2419588d612a6a3be9317c6db63a1c7bc9
jihye byun <jhye.byun@gmail.com>
2023-10-04 16:20:06 +0900

    style: 사용하지 않는 클래스 import 제거

--- airbank-be/src/main/java/com/pangpang/airbank/domain/account/service/AccountServiceImpl.java
+++ airbank-be/src/main/java/com/pangpang/airbank/domain/account/service/AccountServiceImpl.java
@@ -1,6 +1,5 @@
 package com.pangpang.airbank.domain.account.service;
 
-import org.hibernate.event.spi.SaveOrUpdateEvent;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
